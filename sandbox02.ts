62fc0466c91e7a09352547c1dc7a025d98c692c1
Edwin Villacis <edwinvillacisfeus@yahoo.com>
2022-04-04 21:35:21 -0500

    TypeScript default values and parameters

---
+++ sandbox02.ts
@@ -0,0 +1,18 @@
+let myName: String = 'John'
+let age = 30;
+let isPrime = false;
+
+// myName = 30;
+myName = "Peter"
+
+//age = "Ann";
+age = 50;
+
+//isPrime = "Yes";
+isPrime = true;
+
+const circumference = (diameter: number) => {
+    return diameter *  Math.PI;
+};
+
+console.log(circumference(8));
