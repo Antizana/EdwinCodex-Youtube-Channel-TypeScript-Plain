67708d37ef1cadc4f7a1a2eae80640819e7ee45f
Edwin Villacis <edwinvillacisfeus@yahoo.com>
2022-04-04 21:36:07 -0500

    TypeScript Arrays and Objects

---
+++ sandbox03.ts
@@ -0,0 +1,37 @@
+// arrays
+let names = ["John", "Ann", "Peter"];
+//names = "Hello";
+
+names.push("Mary");
+//names.push(7);
+//names[0] = true;
+
+let numbers = [10, 20, 30, 40];
+numbers.push(50);
+// numbers.push("Peter");
+// numbers[1]="Ann"
+
+let stringsAndNumbersArray = ["Mary", 20, "Peter", 31, "John", 27];
+stringsAndNumbersArray.push(28);
+stringsAndNumbersArray.push("Ann");
+stringsAndNumbersArray[2] = 17;
+
+
+// objects
+let person = {
+    name: "John",
+    country: "UK",
+    age: 20
+};
+
+person.age = 25;
+person.country = "USA";
+//person.country = 30;
+// person.city = ["NY", "Los Angeles"];
+
+person = {
+    name: "Ann",
+    country: "USA",
+//    age: 26,
+    age: 21,
+}
