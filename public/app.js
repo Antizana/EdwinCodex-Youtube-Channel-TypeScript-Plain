25b6b50ed8469595289e2e781fc13efdfc795803
Edwin Villacis <edwinvillacisfeus@yahoo.com>
2022-04-06 00:25:57 -0500

    TypeScript Tuples

--- public/app.js
+++ public/app.js
@@ -12,42 +12,29 @@ const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [toFrom.value, details.value, amount.valueAsNumber];
     let document;
     if (type.value === 'invoice') {
-        document = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+        document = new Invoice(...values);
     }
     else {
-        document = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+        document = new Payment(...values);
     }
     list.render(document, type.value, 'end');
 });
-// ENUMS
-var ResourceType;
-(function (ResourceType) {
-    ResourceType[ResourceType["BOOK"] = 1] = "BOOK";
-    ResourceType[ResourceType["AUTHOR"] = 2] = "AUTHOR";
-    ResourceType[ResourceType["FILM"] = 3] = "FILM";
-    ResourceType[ResourceType["DIRECTOR"] = 4] = "DIRECTOR";
-    ResourceType[ResourceType["PERSON"] = 5] = "PERSON";
-})(ResourceType || (ResourceType = {}));
-const document1 = {
-    uid: 1,
-    resourceType: 3,
-    data: { title: 'name of the wind' }
-};
-const document2 = {
-    uid: 2,
-    resourceType: 4,
-    data: { name: 'John Doe' }
-};
-const resource3 = {
-    uid: 3,
-    resourceType: ResourceType.BOOK,
-    data: { title: 'The Final Empire' }
-};
-const resource4 = {
-    uid: 4,
-    resourceType: ResourceType.PERSON,
-    data: { name: 'John Doe' }
-};
-console.log(document1, document2, resource3, resource4);
+// TUPLES
+let array = ["John", 25, true];
+array[0] = false;
+array[1] = "Pete";
+array = [100, false, "John"];
+let myTuple;
+myTuple = ["John", 25, true];
+// myTuple = [40, "Mary", true];
+// myTuple[0] = false;
+myTuple[0] = "Pete";
+// myTuple[1] = "Pete";
+myTuple[1] = 30;
+let student;
+student = ["John", 25];
+// student = [20, "Mary"];
