6b27d3b8572682661c9ad164fd1c115e1b1c5d43
Edwin Villacis <edwinvillacisfeus@yahoo.com>
2022-04-05 22:31:26 -0500

    TypeScript Modules

--- public/app.js
+++ public/app.js
@@ -1,17 +1,9 @@
-"use strict";
+import { Invoice } from "./classes/Invoice.js";
 const button = document.querySelector("button");
-console.log(button);
-// console.log(button.innerText);
-if (button) {
-    console.log(button.innerText);
-}
-console.log(button === null || button === void 0 ? void 0 : button.innerText);
 const button2 = document.querySelector("button");
-console.log(button2.innerText);
 const form = document.querySelector("form");
 const form2 = document.querySelector(".new-item-form");
 const form3 = document.querySelector(".new-item-form");
-console.log(form3.children);
 //Inputs 
 const type = document.querySelector('#type');
 const toFrom = document.querySelector('#tofrom');
@@ -22,45 +14,12 @@ form.addEventListener('submit', (e) => {
     console.log(type.value, toFrom.value, details.value, amount.value, amount.valueAsNumber);
 });
 // Classes
-class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
-    format() {
-        // this.client = "Microsoft"; // this does not work because it is a readonly property client
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
 const invoice1 = new Invoice('Google', 'Web Development', 500);
 const invoice2 = new Invoice('Max', 'Computer', 200);
 let invoices = [];
 invoices.push(invoice1);
 invoices.push(invoice2);
-// invoices.forEach((invoice) => {
-//     console.log(invoice.client, invoice.details, invoice.amount, invoice.format()); //does not work because of private property details
-// });
 invoices.forEach((invoice) => {
     // invoice.client = 'Facebook'; // can't assign a value because it's readonly
     console.log(invoice.client, invoice.amount, invoice.format());
 });
-class OtherInvoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    //Without modifiers this won't work
-    // constructor(
-    //     client: string,
-    //     details: string,
-    //     amount: number,
-    // ){}
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
-const otherInvoice1 = new OtherInvoice('Google', 'Web Development', 500);
-const otherInvoice2 = new OtherInvoice('Microsoft', 'Computer', 200);
-console.log(otherInvoice1.format(), otherInvoice2.format());
